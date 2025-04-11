ca0566f3ff85d6c4bf276a1047b540c89cc01b4c
root <root@haru-2024>
2025-04-11 22:06:58 +0900

    Add Gemfile for deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-devise_for :users 
-root 'top#index'
- get '/explanation', to: 'static_pages#explanation', as: 'explanation' # ①解説ページへのルーティング
- get '/sample', to: 'static_pages#sample', as: 'sample'         # ②sampleページへのルーティング
- get '/login', to: 'users/sessions#new', as: 'login'             # ③ログインページへのルーティング
- get '/signup', to: 'users/registrations#new', as: 'signup'      # ④新規登録ページへのルーティング
+  devise_for :users
+  
+  root 'top#index'
 
+  get '/explanation', to: 'static_pages#explanation', as: 'explanation' # ①解説ページ
+  get '/sample', to: 'static_pages#sample', as: 'sample'                # ②サンプルページ
+  get '/login', to: 'users/sessions#new', as: 'login'                   # ③ログインページ
+  get '/signup', to: 'users/registrations#new', as: 'signup'            # ④新規登録ページ
 end
