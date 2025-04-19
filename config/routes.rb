9f0bf91d0278e557a4cd2845ded07277b8687c6e
root <root@haru-2024>
2025-04-19 18:38:33 +0900

    ruby ver re

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # Deviseのルーティング（コントローラを自作してる場合はcontrollersオプションもつける）
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  # devise_scope で /login や /signup のカスタムルートを設定
+  devise_scope :user do
+    get '/login', to: 'users/sessions#new', as: 'login'
+    get '/signup', to: 'users/registrations#new', as: 'signup'
+  end
 
   # 固定ページ系
-  root 'top#index'  # ← 最初に表示するページ
+  root 'top#index'
   get '/explanation', to: 'static_pages#explanation', as: 'explanation'
   get '/sample', to: 'static_pages#sample', as: 'sample'
-  get '/login', to: 'users/sessions#new', as: 'login'
-  get '/signup', to: 'users/registrations#new', as: 'signup'
 
   # 日記のメイン機能
   resources :diary_entries do
     collection do
-      get :calendar        # カレンダーページ
-      get :summary         # 要約ページ
+      get :calendar
+      get :summary
     end
   end
 
