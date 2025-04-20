b558ed598609d3f06fd7696f9407b519f7f6e380
root <root@haru-2024>
2025-04-20 12:24:16 +0900

    happiness_scores_controller_re

--- app/controllers/happiness_scores_controller.rb
+++ app/controllers/happiness_scores_controller.rb
@@ -1,26 +1,27 @@
-def stats
-  range = case params[:period]
-          when 'week'
-            1.week.ago.to_date..Date.today
-          when 'month'
-            1.month.ago.to_date..Date.today
-          when '3months'
-            3.months.ago.to_date..Date.today
-          when '6months'
-            6.months.ago.to_date..Date.today
-          else
-            1.month.ago.to_date..Date.today
-          end
+class HappinessScoresController < ApplicationController
+  def stats
+    range = case params[:period]
+            when 'week'
+              1.week.ago.to_date..Date.today
+            when 'month'
+              1.month.ago.to_date..Date.today
+            when '3months'
+              3.months.ago.to_date..Date.today
+            when '6months'
+              6.months.ago.to_date..Date.today
+            else
+              1.month.ago.to_date..Date.today
+            end
 
-  @scores = HappinessScore.where(created_at: range)
+    @scores = HappinessScore.where(created_at: range)
+    @average = @scores.average(:score)&.round(2)
+    @max = @scores.maximum(:score)
+    @min = @scores.minimum(:score)
 
-  @average = @scores.average(:score)&.round(2)
-  @max = @scores.maximum(:score)
-  @min = @scores.minimum(:score)
-
-  @chart_data = @scores.group_by { |s| s.created_at.to_date }.map do |date, records|
-    avg_score = records.map(&:score).sum / records.size.to_f
-    memo = records.first.memo.presence || "（メモなし）"
-    { date: date, score: avg_score.round(2), memo: memo }
+    @chart_data = @scores.group_by { |s| s.created_at.to_date }.map do |date, records|
+      avg_score = records.map(&:score).sum / records.size.to_f
+      memo = records.first.memo.presence || "（メモなし）"
+      { date: date, score: avg_score.round(2), memo: memo }
+    end
   end
 end
